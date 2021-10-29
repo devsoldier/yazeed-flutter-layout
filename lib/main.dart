f00a62d37ce65bcb727a06016dc538ba401a209c
yazeed <yazeedwong@gmail.com>
2021-10-29 10:24:24 +0800

    iz working

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,170 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:yazeed_ex4/jasonkeyvalue.dart';
+import 'package:flutter/services.dart' as rootBundle;
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'hehe',
-      home: HomePage(),
+    return MaterialApp(
+      title: 'exercise 4',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+class MyHomePage extends StatefulWidget {
   @override
-  _HomePageState createState() => _HomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
-  List _items = [];
-
-  Future<void> readJson() async {
-    final String response =
-        await rootBundle.loadString('assets/MOCK_DATA.json');
-    final data = await json.decode(response);
-    setState(() {
-      _items = data;
-    });
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text('HAHA')),
-      body: Padding(
-        padding: const EdgeInsets.all(25),
-        child: Column(
-          children: [
-            ElevatedButton(child: const Text('Load Data'), onPressed: readJson),
-            _items.isNotEmpty
-                ? Expanded(
-                    child: ListView.builder(
-                      itemCount: _items.length,
-                      itemBuilder: (context, id) {
-                        return Card(
-                            margin: const EdgeInsets.all(10),
-                            child: ListTile(
-                                leading: Text(_items[id]["first_name"]),
-                                title: Text(_items[id]["last_name"])));
-                      },
-                    ),
-                  )
-                : Container()
-          ],
+        appBar: AppBar(
+          elevation: 5,
+          title: const Text(
+            "deriv's most wanted",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: ReadJsonData(),
+          builder: (context, data) {
+            if (data.hasError) {
+              return Center(child: Text("${data.error}"));
+            } else if (data.hasData) {
+              var items = data.data as List<UserDataModel>;
+              return ListView.builder(
+                  itemCount: items == null ? 0 : items.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 3,
+                      margin: EdgeInsets.symmetric(horizontal: 8, vertical: 5),
+                      child: Container(
+                        padding: EdgeInsets.all(8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 60,
+                              height: 60,
+                              child: Image(
+                                image: NetworkImage(
+                                    items[index].avatar.toString()),
+                                fit: BoxFit.fill,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.only(left: 10),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    // First name
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.only(left: 8, right: 8),
+                                      child: Text(
+                                        items[index].first_name.toString() +
+                                            " " +
+                                            items[index].last_name.toString(),
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            color: Colors.deepPurple[900],
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    // Username
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.only(left: 8, right: 8),
+                                      child: Text(
+                                        items[index].username.toString(),
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ),
+                                    // Status
+                                    Padding(
+                                      padding:
+                                          EdgeInsets.only(left: 8, right: 8),
+                                      child: Text("Status : " +
+                                          items[index].status.toString()),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Container(
+                              width: 80,
+                              height: 60,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  // Lastseen
+                                  Padding(
+                                    padding: EdgeInsets.only(left: 8, right: 8),
+                                    child: Text(
+                                        items[index].last_seen_time.toString()),
+                                  ),
+                                  // Number of chats
+                                  Padding(
+                                    padding: EdgeInsets.only(
+                                        top: 8, left: 8, right: 8),
+                                    child: Container(
+                                      alignment: Alignment.center,
+                                      child: Text(
+                                        items[index].id.toString(),
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            fontSize: 16,
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      width: 30,
+                                      height: 30,
+                                      decoration: new BoxDecoration(
+                                        color: Colors.purple[300],
+                                        shape: BoxShape.circle,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  });
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
+  }
+
+  Future<List<UserDataModel>> ReadJsonData() async {
+    final jsondata =
+        await rootBundle.rootBundle.loadString('assets/MOCK_DATA.json');
+    final list = json.decode(jsondata) as List<dynamic>;
+
+    return list.map((e) => UserDataModel.fromJson(e)).toList();
   }
 }
